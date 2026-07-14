edd9effc118b7ad4ce5fb9c6d260a043841ec0bf
cham207388 <cham207388@gmail.com>
2026-07-14 16:49:36 -0500

    format

--- student-portal-api/src/main/java/com/abc/studentportal/common/application/DynamoStudentCourseQueries.java
+++ student-portal-api/src/main/java/com/abc/studentportal/common/application/DynamoStudentCourseQueries.java
@@ -9,5 +9,6 @@ import java.util.UUID;
 
 public interface DynamoStudentCourseQueries {
 	CursorPage<Course> findCoursesByStudent(UUID studentId, CursorRequest request);
+
 	CursorPage<Student> findStudentsByCourse(UUID courseId, CursorRequest request);
 }
